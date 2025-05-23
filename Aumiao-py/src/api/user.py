107d339b010e88f1ba3937d54c9dc63460ce8433
Aurzex <zybqw@qq.com>
2025-05-23 22:34:20 +0800

    你我生来时就注定天真伟大~

--- Aumiao-py/src/api/user.py
+++ Aumiao-py/src/api/user.py
@@ -88,8 +88,8 @@ class Obtain:
 		response = self.acquire.send_request(endpoint="/nemo/v3/user/level/info", method="GET")
 		return response.json()
 
-	# 获取用户姓名
-	def get_data_name(self) -> dict:
+	# 可能会返回{"code":6404,"msg":"未绑定手机号"},从而可以检测是否绑定手机号
+	def get_data_lesson_info(self) -> dict:
 		response = self.acquire.send_request(endpoint="/api/v2/pc/lesson/user/info", method="GET")
 		return response.json()
 
