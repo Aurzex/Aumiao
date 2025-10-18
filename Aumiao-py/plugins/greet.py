f88a4a51719cdcaf5893c8a00f4e663e809fd945
Aurzex <zybqw@qq.com>
2025-10-19 00:31:53 +0800

    chore: :white_check_mark: 将版本号更新至2.3.0

--- Aumiao-py/plugins/greet.py
+++ Aumiao-py/plugins/greet.py
@@ -11,7 +11,7 @@ class Plugin(BasePlugin):
 
 	@property
 	def PLUGIN_DESCRIPTION(self) -> str:
-		return "一个简单的问候插件，提供多种问候功能"
+		return "一个简单的问候插件,提供多种问候功能"
 
 	@property
 	def PLUGIN_VERSION(self) -> str:
@@ -45,27 +45,23 @@ class Plugin(BasePlugin):
 		"""向指定的人问候"""
 		greeting = self.config.get("default_greeting", "Hello")
 		message = f"{greeting}, {name}!"
-
 		if self.config.get("use_uppercase", False):
 			message = message.upper()
-
 		print(message)
 		return message
 
 	def greet_many(self, name1: str, name2: str = "", name3: str = "") -> list[str]:
 		"""向多个人问候
-
 		Args:
-		    name1: 第一个人的名字 (必需)
-		    name2: 第二个人的名字 (可选)
-		    name3: 第三个人的名字 (可选)
+			name1: 第一个人的名字 (必需)
+			name2: 第二个人的名字 (可选)
+			name3: 第三个人的名字 (可选)
 		"""
 		names = [name1]
 		if name2:
 			names.append(name2)
 		if name3:
 			names.append(name3)
-
 		results = []
 		for name in names:
 			results.append(self.greet(name))
@@ -73,16 +69,13 @@ class Plugin(BasePlugin):
 
 	def repeat_greeting(self, name: str = "World", count: int = 1) -> list[str]:
 		"""重复问候指定次数
-
 		Args:
-		    name: 要问候的人名
-		    count: 重复次数 (最大不超过配置中的max_repeat_count)
+			name: 要问候的人名
+			count: 重复次数 (最大不超过配置中的max_repeat_count)
 		"""
 		max_count = self.config.get("max_repeat_count", 3)
 		actual_count = min(count, max_count)
-
 		results = []
 		for _ in range(actual_count):
 			results.append(self.greet(name))
-
 		return results
